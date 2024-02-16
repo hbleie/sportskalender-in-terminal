0d09b05c40523fb82c4fb28c35b8aa538049f8e0
Håkon Bleie <lfcbleia@gmail.com>
2024-02-16 09:50:33 +0100

    wip(): make it as a zsh plugin

---
+++ sportskalender.plugin.zsh
@@ -0,0 +1,15 @@
+# myplugin.plugin.zsh
+
+function sport() {
+  # Change to the script's directory
+  cd "$(dirname "$0")"
+
+  node hoydepunkter.js
+}
+
+function fotball() {
+  # Change to the script's directory
+  cd "$(dirname "$0")"
+
+  node fotball.js
+}
