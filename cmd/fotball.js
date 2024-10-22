32630654a23b642bdf4e49b1e1795a49ed3eb608
Håkon Bleie <lfcbleia@gmail.com>
2024-10-22 22:22:22 +0200

    Revert "feat: Transform to go"
    This reverts commit 0d35ceed861fe32ebc5ae53a1369b5c8b7d19ef7.

--- cmd/fotball.js
+++
@@ -1,8 +0,0 @@
-import { sportskalender } from "./parse-events.js";
-
-(async () => {
-	const websiteURL = "https://api.vg.no/sportskalender/v1/events?fromDate=";
-	console.log("VGs fotballkalender:");
-
-	sportskalender(websiteURL, "fotball");
-})();
